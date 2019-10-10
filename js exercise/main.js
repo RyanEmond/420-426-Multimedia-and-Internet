284eb67354482c1b4a818be5879c578b39b050e6
Ryan Emond <1822210@johnabbottcollege.net>
2019-10-10 10:55:57 -0400

    q8 done

--- js exercise/main.js
+++ js exercise/main.js
@@ -69,7 +69,33 @@ function Q7(Arr)
 {
     let myArray = Arr;
     let size = myArray.length;
-    let countArray = ([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+    let countArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+    for (let i in myArray) {
+        for (let j = 0; j < size;j++){
+            if (myArray[i] == myArray[j]){
+                countArray[i]++;
+            }
+        }
+    }
+    
+    let max = 0;
+    let index = 0;
+    for (let i in countArray) {
+        if(countArray[i]>max){
+            max = myArray[i];
+            index = i;
+        }
+    }
+    return myArray[index]
+}
+
+
+function Q8(Arr)
+{
+    let myArray = Arr;
+    let size = myArray.length;
+    let countArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
     for (let i in myArray) {
         for (let j = 0; j < size;j++){
@@ -79,16 +105,14 @@ function Q7(Arr)
         }
     }
 
-    let max = 0;
-    let index = 0;
-    for (let i in countArray) {
-        if(countArray[i]>max){
-            max = countArray[i];
-            index = i;
+    for (let i = 0; i<size;i++) {
+        if (countArray[i] > 1) {
+            console.log(myArray[i]);
         }
     }
-    
-    return myArray[index]
+}
 
-    
+funcion Q9()
+{
+
 }
