594bf65cf1179c6a8e27e84388073cebe8b440c3
Ryan Emond <1822210@johnabbottcollege.net>
2019-10-16 14:21:15 -0400

    Q1 halfway done

--- Assignment-2/Assignment-2-RYAN-EMOND.js
+++ Assignment-2/Assignment-2-RYAN-EMOND.js
@@ -1,35 +1,50 @@
 function xoReferee([row1,row2,row3]){
-    let winner = "D";
-
     //columns X
     for(let i = 0; i<3; i++){
         if(row1.charAt(i)=='X' && row2.charAt(i)=='X' && row3.charAt(i)=='X'){
-            winner = "X";
+            return "X";
         }
     }
     //rows X
     if(row1=="XXX" || row2=="XXX" || row3=="XXX"){
-        winner = "X";
+        return "X";
     }
     //diagonal X
     if((row1.charAt(0)=="X" && row2.charAt(1)=="X" && row3.charAt(2)=="X") || (row1.charAt(2)=="X" && row2.charAt(1)=="X" && row3.charAt(0)=="X")){
-        winner = "X";
+        return "X";
     }
 
     //columns O
     for(let i = 0; i<3; i++){
         if(row1.charAt(i)=='O' && row2.charAt(i)=='O' && row3.charAt(i)=='O'){
-            winner = "O";
+            return "O";
         }
     }
     //rows O
     if(row1=="OOO" || row2=="OOO" || row3=="OOO"){
-        winner = "O";
+        return "O";
     }
     //diagonal O
     if((row1.charAt(0)=="O" && row2.charAt(1)=="O" && row3.charAt(2)=="O") || (row1.charAt(2)=="O" && row2.charAt(1)=="O" && row3.charAt(0)=="O")){
-        winner = "O";
+        return "O";
     }
+    return "D";
+}
 
-    console.log(winner);
+function safePawns(pawns = []){
+    
+    let size = pawns.length;
+    let safeCount = 0;
+    for(let i = 0; i < size; i++){
+        let x = pawns[i].charAt(0);
+        let y = pawns[i].charAt(1);
+        for(let j = 0; j < size; j++){
+            let xx = pawns[j].charAt(0);
+            let yy = pawns[j].charAt(1);
+            if(yy==y-1 && (xx==x-1 || xx==x+1)){
+                safeCount = safeCount + 1;
+            }
+        }
+    }
+    return safeCount;
 }
