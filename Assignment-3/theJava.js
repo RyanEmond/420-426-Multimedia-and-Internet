ab18cd2fc704f439604708dc6282bc3412598bda
Ryan Emond <1822210@johnabbottcollege.net>
2019-11-04 14:08:20 -0500

    a4 worked on

--- Assignment-3/theJava.js
+++ Assignment-3/theJava.js
@@ -1,5 +1,35 @@
-const url = "https://api.spacexdata.com/v3/rockets"; // The root of the SpaceX API
+const url = "https://api.spacexdata.com/v3/"; // The root of the SpaceX API
+const theNext = "https://api.spacexdata.com/v3/launches/next"
+const rockets = "https://api.spacexdata.com/v3/rockets";
+const thePast = "https://api.spacexdata.com/v3/launches/past?limit=3"
 fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        doStuff(data)
+    })
+    .catch(function(error) {
+        // If there is any error you will catch them here.
+        console.log(error);
+});
+fetch(theNext)
+    .then(response => response.json())
+    .then(data => {
+        doStuff(data)
+    })
+    .catch(function(error) {
+        // If there is any error you will catch them here.
+        console.log(error);
+});
+fetch(rockets)
+    .then(response => response.json())
+    .then(data => {
+        doStuff(data)
+    })
+    .catch(function(error) {
+        // If there is any error you will catch them here.
+        console.log(error);
+});
+fetch(thePast)
     .then(response => response.json())
     .then(data => {
         doStuff(data)
