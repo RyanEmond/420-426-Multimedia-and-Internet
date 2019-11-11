bd726301330cb9b377e3873b00978f6cb7557b4b
Ryan Emond <1822210@johnabbottcollege.net>
2019-11-11 14:13:40 -0500

    paths done

--- 7.2-Starting-With-Canvas/main.js
+++ 7.2-Starting-With-Canvas/main.js
@@ -1,6 +1,18 @@
 const canvas = document.getElementsByTagName('canvas')[0];
 const context = canvas.getContext('2d');
 
+/*
 context.fillRect(20,20,100,100);
 context.clearRect(40,40,60,60);
 context.strokeRect(45,45,50,50);
+*/
+
+/*
+context.beginPath();
+context.moveTo(75, 50);
+context.lineTo(100,75);
+context.lineTo(100,25);
+context.closePath();
+context.fill();
+*/
+
