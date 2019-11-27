0edc0ac9e91051e3cb1af4fd8e43bf9254e3db56
Ryan Emond <1822210@johnabbottcollege.net>
2019-11-27 14:22:24 -0500

    blocks started, speed fixed

--- Game/classes.js
+++ Game/classes.js
@@ -2,8 +2,12 @@ class Character{
     constructor(x,y){
         this.x = x;
         this.y = y;
-        this.speed = 10;
+        this.speed = 5;
         this.size = 40;
+        this.left = false;
+        this.right = false;
+        this.up = false;
+        this.down = false;
     }
     draw(){
         context.save();
@@ -44,6 +48,22 @@ class Character{
         context.font = "12px Arial";
         context.fillText(`Position: (${this.x.toFixed(0)},${this.y.toFixed(0)})`, 10, 20);
         context.fillText(`Speed: ${this.speed.toFixed(2)}`, 10, 40);
+        context.restore();
+    }
+}
+class Collectable{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.colours = ["Green", "Yellow", "Blue"];
+        this.colour = this.colours[Math.floor(Math.random() * Math.floor(3))];
+        this.size = 20;
+    }
+    draw(){
+        context.save();
+        context.translate(this.x, this.y);
+        context.fillStyle = this.colour;
+        context.fillRect(0, 0, this.size, this.size);
         context.restore();
     }
 }
--- Game/main.js
+++ Game/main.js
@@ -2,29 +2,60 @@ let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
 let character = new Character(canvas.width / 2 - 20, canvas.height / 2 - 20);
 
-animate();
 canvas.focus();
 window.addEventListener("keydown", keyDown);
+window.addEventListener("keyup", keyUp);
 
+animate();
 
 function animate(){
     requestAnimationFrame(animate);
     context.clearRect(0,0,canvas.width,canvas.height);
     character.displayStatus();
+    movement();
     character.update();
+    
+    
 }
 function keyDown(e){
     if(e.keyCode == '37'){          //left
-        character.goLeft();
+        character.left = true;
     }
     else if(e.keyCode == '38'){     //up
-        character.goUp();
+        character.up = true;
     }
     else if(e.keyCode == '39'){     //right
-        character.goRight();
+        character.right = true;
     }
     else if(e.keyCode == '40'){     //down
+        character.down = true;
+    }
+}
+function keyUp(e){
+    if(e.keyCode == '37'){          //left
+        character.left = false;
+    }
+    else if(e.keyCode == '38'){     //up
+        character.up = false;
+    }
+    else if(e.keyCode == '39'){     //right
+        character.right = false;
+    }
+    else if(e.keyCode == '40'){     //down
+        character.down = false;
+    }
+}
+function movement(){
+    if(character.left){
+        character.goLeft();
+    }
+    if(character.right){
+        character.goRight();
+    }
+    if(character.up){
+        character.goUp();
+    }
+    if(character.down){
         character.goDown();
     }
-    animate();
 }
